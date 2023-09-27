ef8701a72323e6577922c69e411c1b7b1203d4ab
Daniel <lenj94002@gmail.com>
2023-09-27 21:24:41 +0200

    REALLY GOOD PROGRESS

--- server/sqlconnection.php
+++ server/sqlconnection.php
@@ -4,7 +4,19 @@ $dbHost = '***REMOVED***';
 $dbPort = ***REMOVED***;
 $dbName = '***REMOVED***';
 $dbUser = '***REMOVED***';
-$dbPassword = '';
+$dbPassword = '***REMOVED***';
+
+//$graphType = $_POST['graphType']; // Get the graphType from the POST request
+//$amount = $_POST['amount']; // Get the amount from the POST request
+//$interval = $_POST['interval']; // Get the interval from the POST request
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$graphType = $data->graphType;
+$amount = $data->amount;
+$interval = $data->interval;
+
+
+
 
 // Create a PDO connection
 try {
@@ -18,7 +30,7 @@ try {
 function getData($graphType, $amount, $interval) {
     global $pdo;
     try {
-        $sql = "SELECT $graphType, timestamp FROM WeatherData WHERE Timestamp > DATE_SUB(NOW(), INTERVAL $amount $interval)";
+        $sql = "SELECT $graphType, time FROM Data WHERE time > DATE_SUB(NOW(), INTERVAL $amount $interval) LIMIT 5;";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -26,5 +38,12 @@ function getData($graphType, $amount, $interval) {
     } catch (PDOException $e) {
         throw $e;
     }
+}
+try {
+    $data = getData($graphType, $amount, $interval);
+    echo json_encode($data);
+} catch (PDOException $e) {
+    // Handle any errors that may occur
+    echo json_encode(['error' => $e->getMessage()]);
 }
 ?>
